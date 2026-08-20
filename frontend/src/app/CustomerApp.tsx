3e74f0551e422051f41de8d82ad33bbf748f0b35
Armando R. Abarado Jr. <abaradoaj@gmail.com>
2026-08-20 18:54:13 +0800

    feat: route About to refined storefront ending

--- frontend/src/app/CustomerApp.tsx
+++ frontend/src/app/CustomerApp.tsx
@@ -1,5 +1,6 @@
 import { CartProvider } from "@/context/CartContext";
 import { CustomerLayout } from "@/layouts/CustomerLayout";
+import { AboutExperiencePage } from "@/pages/customer/AboutExperiencePage";
 import { CartPage } from "@/pages/customer/CartPage";
 import { CheckoutPage } from "@/pages/customer/CheckoutPage";
 import { CustomerHomePage } from "@/pages/customer/CustomerHomePage";
@@ -43,7 +44,7 @@ export function CustomerApp({
   else if (pathname === "/checkout") page = <CheckoutPage navigate={navigate} />;
   else if (pathname === "/order-success")
     page = <OrderSuccessPage location={location} navigate={navigate} />;
-  else if (pathname === "/about") page = <DiscoverPage navigate={navigate} />;
+  else if (pathname === "/about") page = <AboutExperiencePage navigate={navigate} />;
   else if (pathname === "/discover") page = <DiscoverPage navigate={navigate} />;
   else page = <CustomerNotFoundPage navigate={navigate} />;
 
