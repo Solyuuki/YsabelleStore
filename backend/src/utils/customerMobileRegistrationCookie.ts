fd42df0904beb930b2bf26a75ac04306c6c8cb42
Armando R. Abarado Jr. <abaradoaj@gmail.com>
2026-08-31 01:03:48 +0800

    style(auth): apply exact registration grant formatting

--- backend/src/utils/customerMobileRegistrationCookie.ts
+++ backend/src/utils/customerMobileRegistrationCookie.ts
@@ -1,9 +1,7 @@
 import type { Request, Response } from "express";
 
 import { env } from "../config/env.js";
-import {
-  CUSTOMER_MOBILE_REGISTRATION_OTP_LIFETIME_MS
-} from "../services/customerMobileRegistrationService.js";
+import { CUSTOMER_MOBILE_REGISTRATION_OTP_LIFETIME_MS } from "../services/customerMobileRegistrationService.js";
 
 export const CUSTOMER_MOBILE_REGISTRATION_COOKIE_NAME = "ysabelle_customer_registration_mobile";
 
